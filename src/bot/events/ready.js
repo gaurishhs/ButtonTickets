34f41b38f381bacf8f370700bdf47db7e49038d0
apidev234 <78847111+apidev234@users.noreply.github.com>
2021-07-10 17:12:47 +0530

    Ready Command But Special. ;-;

---
+++ src/bot/events/ready.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+const figlet = require("figlet");
+
+module.exports = async function(client) {
+    console.log(chalk.yellow.bold(figlet.textSync("8 Ticket")));
+    await console.log(chalk.red.bold(client.user.tag) + chalk.blue.bold(" Is Ready"));
+
+    await client.user.setActivity(require('../../config/bot').prefix + "help || Made By Shinchan#2196"); // Don't Remove Credits Might Get You in Danger
+    await client.user.setStatus("idle");
+}
