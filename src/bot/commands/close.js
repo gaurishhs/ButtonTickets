f95ef631f7a5deebd3995a96162ae503febf52c4
apidev234 <78847111+apidev234@users.noreply.github.com>
2021-07-10 17:04:39 +0530

    Close a Ticket

---
+++ src/bot/commands/close.js
@@ -0,0 +1,52 @@
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+
+module.exports = {
+    name: "close",
+    cooldown: 5,
+    aliases: ["end"],
+
+    run: async function(client, message, args) {
+        try {
+            if (!message.channel.name.includes("ticket-")) {
+                message.channel.send({
+                    embed: {
+                        title: `**❌ | Error**`,
+                        description: `This is not a ticket channel`,
+                        color: 0xFF0000
+                    }
+                }).then(async function(msg) {
+                    setTimeout(() => {
+                        msg.delete().catch(err => { return })
+                    }, 1000 * 7);
+                })
+                return
+            }
+            let btn = new MessageButton()
+                .setStyle("green")
+                .setEmoji("✅")
+                .setID("closeTicketTrue");
+            let btn2 = new MessageButton()
+                .setStyle("grey")
+                .setEmoji("⛔")
+                .setID("closeTicketFalse");
+            let row = new MessageActionRow()
+                .addComponent(btn)
+                .addComponent(btn2);
+            message.channel.send({
+                embed: {
+                    title: `**⚠️ | Confirmation**`,
+                    description: `Are you sure about closing this ticket?`,
+                    color: 0xFFF200
+                },
+                component: row
+            }).then(async function(msg) {
+                setTimeout(() => {
+                    msg.delete().catch(err => { return })
+                }, 1000 * 7);
+                require('quick.db').set(`DeleteMessage_${message.channel.id}`, msg.id)
+            })
+        } catch (err) {
+            return;
+        }
+    }
+}
