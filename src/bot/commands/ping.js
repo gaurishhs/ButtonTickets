5a76dff1e4a30b1caac827cf84b36699c4d90d40
apidev234 <78847111+apidev234@users.noreply.github.com>
2021-07-10 17:06:59 +0530

    Ping Command But Special

---
+++ src/bot/commands/ping.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    cooldown: 7,
+    aliases: ["ping"],
+    run: async(client, message) => {
+        try {
+            var states = "🟢 Excellent";
+            var states2 = "🟢 Excellent";
+            var msg = `${Date.now() - message.createdTimestamp}`;
+            var api = `${Math.round(client.ws.ping)}`;
+            if (Number(msg) > 70) states = "🟢 Good";
+            if (Number(msg) > 170) states = "🟡 Not Bad";
+            if (Number(msg) > 350) states = "🔴 Soo Bad";
+            if (Number(api) > 70) states2 = "🟢 Good";
+            if (Number(api) > 170) states2 = "🟡 Not Bad";
+            if (Number(api) > 350) states2 = "🔴 Soo Bad";
+            if (message.author.bot) return;
+            message.channel.send(
+                new MessageEmbed()
+                .setColor("#2F3136")
+                .setAuthor(message.author.username, message.author.avatarURL())
+                .addField("**Time Taken:**", msg + " ms 📶 | " + states, true)
+                .addField("**WebSocket:**", api + " ms 📶 | " + states2, true)
+                .setTimestamp()
+                .setFooter(`Request By ${message.author.tag}`)
+            );
+        } catch (err) {
+            return;
+        }
+    }
+};
