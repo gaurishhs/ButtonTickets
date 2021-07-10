03d3e99e1321ab5a72b746b3d9a2bfb787c1744d
apidev234 <78847111+apidev234@users.noreply.github.com>
2021-07-10 17:07:58 +0530

    Remove Person From Ticket

---
+++ src/bot/commands/remove.js
@@ -0,0 +1,58 @@
+module.exports = {
+    name: "remove",
+    cooldown: 6,
+    aliases: ['get-out'],
+
+    run: async function(client, message, args) {
+        if (!message.channel.name.includes("ticket-")) {
+            message.channel.send({
+                embed: {
+                    title: `**❌ | Error**`,
+                    description: `This is not a ticket channel`,
+                    color: 0xFF0000
+                }
+            }).then(async function(msg) {
+                setTimeout(() => {
+                    msg.delete().catch(err => { return })
+                }, 1000 * 7);
+            })
+            return
+        }
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) {
+            message.channel.send({
+                embed: {
+                    title: `**❌ | Error**`,
+                    description: `you need same permissions to use this command`,
+                    color: 0xFF0000
+                }
+            }).then(async function(msg) {
+                setTimeout(() => {
+                    msg.delete().catch(err => { return })
+                }, 1000 * 7);
+            })
+            return
+        }
+        message.channel.overwritePermissions([{
+            id: require('quick.db').fetch(`TicketControl_${message.channel.id}`),
+            allow: ['SEND_MESSAGES', 'VIEW_CHANNEL']
+        }, {
+            id: require('quick.db').fetch(`TicketAdminRole_${message.guild.id}`),
+            allow: ["SEND_MESSAGES", "VIEW_CHANNEL"]
+        }, {
+            id: message.guild.roles.everyone,
+            deny: ["VIEW_CHANNEL"]
+        }]).then(() => {
+            message.channel.send({
+                embed: {
+                    title: '✅ | Done',
+                    description: `the ticket permission has been rested to default`,
+                    color: 0x00D700
+                }
+            }).then(async function(msg) {
+                setTimeout(() => {
+                    msg.delete().catch(err => { return })
+                }, 1000 * 7);
+            })
+        })
+    }
+}
